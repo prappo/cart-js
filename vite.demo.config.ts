83ba46e4e9a9f42876481f8bc2c3ade9e3ca68b8
Prappo <prappo.prince@gmail.com>
2024-11-08 23:41:55 +0600

    add demo

--- vite.demo.config.ts
+++ vite.demo.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export default defineConfig({
   base: '/cart-js/', // Replace with your repo name
   build: {
-    outDir: 'demo',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html')
